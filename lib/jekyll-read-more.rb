56d8ce2cc5f5bf0c963ec2b7c9b33d236e8523af
Rob Shaw <rob@borwahs.com>
2015-03-09 22:43:43 -0400

    Create initial version of the plugin.
    - Add method to see if HTML tag is in post.
    - Add method to retrieve excerpt.
    - Strip out footnotes from excerpt using <sub> tag.

---
+++ lib/jekyll-read-more.rb
@@ -0,0 +1,1 @@
+require 'jekyll-read-more/filter'
---
+++ lib/jekyll-read-more/filter.rb
@@ -0,0 +1,43 @@
+# jekyll-read-more
+#
+# A Liquid Filter for Jekyll to check and retrieve
+# an excerpt from a post.
+#
+# https://github.com/borwahs/jekyll-post-excerpt
+#
+# Copyright (c) Rob Shaw, 2015
+# See readme.md for LICENSE information.
+
+module Jekyll
+  module ReadMore
+    module Filter
+      extend self
+
+      # uses the markdown format <!--- (three dashes)
+      EXCERPT_BREAK_TAG = "<!---excerpt-break-->"
+
+      def post_contains_excerpt_tag?(post)
+        post.include?(EXCERPT_BREAK_TAG)
+      end
+
+      def get_post_excerpt(post)
+        if !post_contains_excerpt_tag?(post)
+          return post
+        end
+
+        post_split = post.split(EXCERPT_BREAK_TAG)
+
+        strip_footnotes(post_split[0])
+      end
+
+      def strip_footnotes(content)
+        # Example: <sup>...</sup>
+        # Regex: <sup>.*?<\/sup>/mi
+
+        content.gsub(/<sup>.*?<\/sup>/mi, '')
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ReadMore::Filter) if defined?(Liquid)
